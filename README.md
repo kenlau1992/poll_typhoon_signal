662f8a3e69b17de23d928cca06d2321696dd39b3
Ken Lau <kenlau19920406@gmail.com>
2016-10-21 03:22:46 +0800

    Allow -h and -t options
    - -h mean help
    - -t to change poll time interval

--- README.md
+++ README.md
@@ -5,8 +5,13 @@ DESCRIPTION
 
 The script gets typhoon singal from HK Observatory (HKO) web page. It will
 print the typhoon singal number to screen in interval specified by variable
-$POLL_INTERVAL in the script. The script stops by Ctrl+C
+$POLL_INTERVAL in the script or user input. The script stops by Ctrl+C
 
 USAGE
 ------------
+Poll with default time interval
 $ ./poll_typhoon_singal
+Poll with user config time interval
+$ ./poll_typhoon_singal -t <seconds>
+Print help
+$ ./poll_typhoon_singal -h
--- poll_typhoon_signal.sh
+++ poll_typhoon_signal.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
 
+PROG_NAME=`basename $0`
 HKO_WEB="http://www.weather.gov.hk/contente.htm"
 # in second
 POLL_INTERVAL=60
+
+print_usage()
+{
+	echo "usage: $PROG_NAME [options]"
+	echo "Options:"
+	echo "         -h [h]elp"
+	echo "         -t Poll [t]ime interval <second>"
+}
+
+# print error message and exit
+error_and_exit()
+{
+	local error_code=$1
+	shift 1
+	if [ ! -z "$1" ]; then
+		echo $@
+	fi
+	exit $error_code
+}
+
+parse_args()
+{
+	until [ -z $1 ]; do
+		case $1 in
+		-t)
+			[ -z "$2" ] && error_and_exit 1 "Input think client directory"
+			if [ $2 -eq $2 2>/dev/null ]; then
+				POLL_INTERVAL=$2
+			else
+				error_and_exit 1 "Input '$2' is not a number"
+			fi
+			shift 2
+			;;
+		-h)
+			print_usage
+			exit 0
+			;;
+		*)
+			error_and_exit 2 "Unrecognized input"
+			;;
+		esac
+	done
+}
 
 get_typhoon_singal()
 {
@@ -11,6 +55,7 @@ get_typhoon_singal()
 	signal_no=`echo $typhoon_singal | egrep -o '[0-9]+'`
 }
 
+parse_args $@
 while : ; do
 	get_typhoon_singal
 	if [ -z $signal_no ]; then
